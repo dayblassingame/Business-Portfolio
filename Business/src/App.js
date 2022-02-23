884191b629b0b9d9b0f2d170e9ba4e35be1aca89
Dazhia Blassingame <dayblassingame@gmail.com>
2022-02-23 14:00:36 -0500

    react and sass setup with webpack

---
+++ Business/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import('./App.scss');
+export function App(){
+    return (
+        <div>
+            <h1 className='dbp'> Welcome</h1>
+        </div>
+    );
+}
---
+++ Business/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, './src/index.js'),
+    devtool: "eval-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                }]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, './src'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        port: 3343,
+        hot: true,
+    },
+};
