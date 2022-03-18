7bf39c0556dc2a22a20d97498ca720d933ec8ad0
Dazhia Blassingame <dayblassingame@gmail.com>
2022-03-18 19:41:04 -0400

    landing page connected

--- Business/src/App.js
+++ Business/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import('./App.scss');
 import {Navbar} from './Navbar';
+import {Landing} from './Landing';
 
 export function App(){
     return (
         <div>
             <Navbar/>
+            <Landing imgSrc ='./images/profile.jpg' />
         </div>
     );
 }
---
+++ Business/src/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Landing() {
+    return(
+        <div>
+            <div>
+                <img src='./images/profile.jpg'/>
+            </div>
+            <div>
+            </div>
+        </div>
+        
+    );
+}
+
