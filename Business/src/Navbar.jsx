0090f504c5143e8ea093a4aff5be830ebb9e892c
Dazhia Blassingame <dayblassingame@gmail.com>
2022-03-31 20:23:52 -0400

    landing page responsive / looks good on all devices

--- Business/src/Navbar.jsx
+++ Business/src/Navbar.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { useState } from "react";
 import { icon } from '@fortawesome/fontawesome-svg-core';
-
+//navigation bar for different pages of site
 
 export function Navbar(){
 const [isActive, setActive] = useState("false");
@@ -13,9 +13,9 @@ const hamburgerMenu = () => {
     setActive(!isActive);
 };
     
-    
     return (
-        <div className='bp-C-TopNavbar_container bp-U-align-content-center'>
+        <div className='bp-C-Navbar_container bp-U-align-content-center'>
+            {/* hamburger menu for navigation on smaller devices */}
             <button id='hamburgerMenu' className='btn bp-C-Hamburger_icon' onClick={hamburgerMenu}><FontAwesomeIcon icon={isActive ? solid('bars') : solid('xmark')} /></button>
             <ul id='sideNavbar' className={isActive ? "bp-C-SideNavbar_list bp-U-display-none": "bp-C-SideNavbar_list"}>
                 <li className='bp-C-SideNavbar_list_items bp-U-display-block'> <a href=''>Home </a></li>
@@ -24,6 +24,8 @@ const hamburgerMenu = () => {
                 <li className='bp-C-SideNavbar_list_items bp-U-display-block'> <a href=''>Portfolio</a></li>
                 <li className='bp-C-SideNavbar_list_items bp-U-display-block'> <a href=''>Contact Us</a></li>
             </ul>
+
+            {/* hamburger menu for navigation on wider devices */}
             <ul id='topNavbar'className='bp-C-TopNavbar_list bp-U-text-align-center'>
                 <li className='bp-C-TopNavbar_list_items bp-U-display-inline-block'> <a href=''>Home </a></li>
                 <li className='bp-C-TopNavbar_list_items bp-U-display-inline-block'> <a href=''>About</a></li>
@@ -33,4 +35,5 @@ const hamburgerMenu = () => {
             </ul>
         </div>
     );
+
 }
